e2fde238f81a04415e787adfc1efb641605be1cc
bekka <beqachachua@gmail.com>
2020-08-10 01:43:18 +0400

    create MatchesAdapter for playerAction visualization

---
+++ app/src/main/java/com/example/test/data/model/PlayersAction.kt
@@ -0,0 +1,9 @@
+package com.example.test.data.model
+
+data class PlayersAction(
+    val playerImage: String,
+    val time: Int,
+    val actionType: String,
+    val PlayerName: String,
+    val playerActionType: String
+)
--- app/src/main/java/com/example/test/ui/matches/MatchesAdapter.kt
+++ app/src/main/java/com/example/test/ui/matches/MatchesAdapter.kt
@@ -1,5 +1,29 @@
 package com.example.test.ui.matches
 
-class MatchesAdapter (){
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.test.data.model.PlayersAction
+
+class MatchesAdapter(val context: Context,val playerList: ArrayList<PlayersAction>):
+    RecyclerView.Adapter<MatchesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
 
 }
